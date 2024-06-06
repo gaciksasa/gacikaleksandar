a4b7e74a537e7db4921e99a93b9d36e7b40241a3
gaciksasa <gacik.mail@gmail.com>
2024-06-06 23:11:54 +0200

    config update

--- config.php
+++ config.php
@@ -28,3 +28,14 @@ if (!defined('DB_NAME')) {
   define('DB_NAME', 'gacikaleksandar');
 }
 
+// Connect to the database
+$conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+
+// Check the connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Set the charset to utf8mb4
+$conn->set_charset("utf8mb4");
+
