82d88571dfe60b421a1da750c6a12d03048287a2
gaciksasa <gacik.mail@gmail.com>
2024-06-06 23:30:32 +0200

    Added language selector to header

--- header.php
+++ header.php
@@ -1,97 +1,119 @@
+<?php
+session_start();
+
+if (isset($_GET['lang'])) {
+    $lang = $_GET['lang'];
+    $_SESSION['lang'] = $lang;
+} elseif (!isset($_SESSION['lang'])) {
+    $_SESSION['lang'] = 'en'; // Default language
+}
+
+$lang = $_SESSION['lang'];
+$translations = include "languages/$lang.php";
+?>
+
 <div class="pbmit-header-overlay">
-				<div class="site-header-menu">
-					<div class="container-fluid p-0">
-						<div class="row g-0">
-							<div class="col-md-12">
-								<div class="d-flex align-items-center justify-content-between">
-									<div class="d-flex justify-content-between align-items-center">
-										<div class="site-branding">
-											<span class="site-title">
-												<a href="index.html">
-													<img class="logo-img" src="images/homepage-1/logo.png" alt="Gimox">
-													<img class="sticky-logo" src="images/homepage-1/logo-dark.png" alt="Gimox">
-												</a>
-											</span>
-										</div>
-										<div class="site-navigation">
-											<nav class="main-menu navbar-expand-xl navbar-light">
-												<div class="navbar-header">
-													<!-- Toggle Button -->
-													<button class="navbar-toggler" type="button">
-														<i class="pbmit-gimox-icon-bars"></i>
-													</button>
-												</div>
-												<div class="pbmit-mobile-menu-bg"></div>
-												<div class="collapse navbar-collapse clearfix show" id="pbmit-menu">
-													<div class="pbmit-menu-wrap">
-														<ul class="navigation clearfix">
-															<li class="dropdown active">
-																<a href="index.php">Home</a>
-															</li>
-															<li class="dropdown">
-																<a href="#">Pages</a>
-																<ul>
-																	<li><a href="about-us.html">About us</a></li>
-																	<li><a href="our-services.html">Our Services</a></li>
-																	<li><a href="our-pricing.html">Our Pricing</a></li>
-																	<li><a href="our-trainers.html">Our Trainers</a></li>
-																	<li><a href="trainer-details.html">Trainer Details</a></li>
-																	<li><a href="faq.html">Faq</a></li>
-																</ul>
-															</li>
-															<li class="dropdown">
-																<a href="#">Portfolio</a>
-																<ul>
-																	<li><a href="portfolio-style-1.html">Portfolio Style 1</a></li>
-																	<li><a href="portfolio-style-2.html">Portfolio Style 2</a></li>
-																	<li><a href="portfolio-single.html">Portfolio Single</a></li>
-																</ul>
-															</li>
-															<li class="dropdown">
-																<a href="classes.html">Classes</a>
-																<ul>
-																	<li><a href="classes-details.html">Classes Details</a></li>
-																</ul>
-															</li>
-															<li class="dropdown">
-																<a href="blog-grid-view.php">Blog</a>
-															</li>
-															<li><a href="contacts.html">Contacts</a></li>
-														</ul>
-													</div>
-												</div>
-											</nav>
-										</div>
-									</div>
-									<div class="pbmit-right-side">
-										<div class="pbmit-social-links-wrapper">
-											<ul class="social-icons">
-												<li class="pbmit-social-facebook">
-													<a class=" tooltip-top" target="_blank" href="#">
-														<i class="pbmit-base-icon-facebook"></i>
-													</a>
-												</li>
-												<li class="pbmit-social-instagram">
-													<a class=" tooltip-top" target="_blank" href="#">
-														<i class="pbmit-base-icon-instagram"></i>
-													</a>
-												</li>
-												<li class="pbmit-social-youtube">
-													<a class=" tooltip-top" target="_blank" href="#">
-														<i class="pbmit-base-icon-youtube"></i>
-													</a>
-												</li>
-											</ul>
-										</div>
-										<div class="pbmit-header-button">
-											<a class=" tooltip-top" href="tel:+381604494033">
-												<i class="pbmit-base-icon-phone"> </i> 060 44 94 033
-											</a>
-										</div>
-									</div>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
+    <div class="site-header-menu">
+        <div class="container-fluid p-0">
+            <div class="row g-0">
+                <div class="col-md-12">
+                    <div class="d-flex align-items-center justify-content-between">
+                        <div class="d-flex justify-content-between align-items-center">
+                            <div class="site-branding">
+                                <span class="site-title">
+                                    <a href="index.php">
+                                        <img class="logo-img" src="images/homepage-1/logo.png" alt="Gimox">
+                                        <img class="sticky-logo" src="images/homepage-1/logo-dark.png" alt="Gimox">
+                                    </a>
+                                </span>
+                            </div>
+                            <div class="site-navigation">
+                                <nav class="main-menu navbar-expand-xl navbar-light">
+                                    <div class="navbar-header">
+                                        <!-- Toggle Button -->
+                                        <button class="navbar-toggler" type="button">
+                                            <i class="pbmit-gimox-icon-bars"></i>
+                                        </button>
+                                    </div>
+                                    <div class="pbmit-mobile-menu-bg"></div>
+                                    <div class="collapse navbar-collapse clearfix show" id="pbmit-menu">
+                                        <div class="pbmit-menu-wrap">
+                                            <ul class="navigation clearfix">
+                                                <li class="dropdown active">
+                                                    <a href="index.php">Home</a>
+                                                </li>
+                                                <li class="dropdown">
+                                                    <a href="#">Pages</a>
+                                                    <ul>
+                                                        <li><a href="about-us.php">About us</a></li>
+                                                        <li><a href="our-services.php">Our Services</a></li>
+                                                        <li><a href="our-pricing.php">Our Pricing</a></li>
+                                                        <li><a href="our-trainers.php">Our Trainers</a></li>
+                                                        <li><a href="trainer-details.php">Trainer Details</a></li>
+                                                        <li><a href="faq.php">Faq</a></li>
+                                                    </ul>
+                                                </li>
+                                                <li class="dropdown">
+                                                    <a href="#">Portfolio</a>
+                                                    <ul>
+                                                        <li><a href="portfolio-style-1.php">Portfolio Style 1</a></li>
+                                                        <li><a href="portfolio-style-2.php">Portfolio Style 2</a></li>
+                                                        <li><a href="portfolio-single.php">Portfolio Single</a></li>
+                                                    </ul>
+                                                </li>
+                                                <li class="dropdown">
+                                                    <a href="classes.php">Classes</a>
+                                                    <ul>
+                                                        <li><a href="classes-details.php">Classes Details</a></li>
+                                                    </ul>
+                                                </li>
+                                                <li class="dropdown">
+                                                    <a href="blog-grid-view.php">Blog</a>
+                                                </li>
+                                                <li><a href="contacts.php">Contacts</a></li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                </nav>
+                            </div>
+                        </div>
+                        <div class="pbmit-right-side">
+                            <div class="pbmit-social-links-wrapper">
+                                <ul class="social-icons">
+                                    <li class="pbmit-social-facebook">
+                                        <a class="tooltip-top" target="_blank" href="#">
+                                            <i class="pbmit-base-icon-facebook"></i>
+                                        </a>
+                                    </li>
+                                    <li class="pbmit-social-instagram">
+                                        <a class="tooltip-top" target="_blank" href="#">
+                                            <i class="pbmit-base-icon-instagram"></i>
+                                        </a>
+                                    </li>
+                                    <li class="pbmit-social-youtube">
+                                        <a class="tooltip-top" target="_blank" href="#">
+                                            <i class="pbmit-base-icon-youtube"></i>
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div>
+							<div class="language-selector">
+                                <form method="get" action="" class="form-inline">
+                                    <select name="lang" onchange="this.form.submit()">
+                                        <option value="en" <?php if($lang == 'en') echo 'selected'; ?>>EN</option>
+                                        <option value="sr" <?php if($lang == 'sr') echo 'selected'; ?>>SR</option>
+                                    </select>
+                                </form>
+                            </div>
+                            <div class="pbmit-header-button">
+                                <a class="tooltip-top" href="tel:+381604494033">
+                                    <i class="pbmit-base-icon-phone"> </i> 060 44 94 033
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
